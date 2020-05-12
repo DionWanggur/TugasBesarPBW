965fc508847826020b3adebb6519c6c0202663dc
Quadrat <56858127+quadratnp@users.noreply.github.com>
2020-05-12 11:36:04 +0700

    Update controller.php

--- Controller/controller.php
+++ Controller/controller.php
@@ -6,7 +6,8 @@ class Controller{
 	public function view_index(){
 		if(session_status() == PHP_SESSION_ACTIVE and isset($_SESSION['nama'])) {
 			$nama = $_SESSION['nama'];
-			if ($nama == "admin") {
+			$kondisi = $_SESSION['kondisi'];
+			if ($kondisi == "admin") {
 				$content = "<h1>Layanan Bagi Admin Untuk Mengatur Jadwal Ujian</h1><br>
 				<img src='View/Img/ujian.jpg' alt='ujian' style='width: 400px;height: 200px'>
 				<h2>Selamat Datang ".$nama."</h2>";
@@ -22,9 +23,27 @@ class Controller{
 	}
 	
 	public function login(){
-		if (isset($_POST['nama']) and $_POST['nama'] != "") {
-			$_SESSION['nama'] = $_POST['nama'];
+		$username = $_POST['nama'];
+		$password = $_POST['password'];
+		$kondisi='';
+
+		if (isset($username) && isset($password) && $username != "" && $password != "") {
+			$query = "SELECT * FROM `user` WHERE `username` LIKE '$username' AND `password` LIKE '$password' ";
+			$query_result = $this->db->executeSelectQuery($query);
+			if($query_result[0]['role']=='mahasiswa'){
+				$kondisi = 'mahasiswa';
+				$_SESSION['username'] = $query_result[0]['username'];
+				$_SESSION['password'] = $query_result[0]['password'];
+				$_SESSION['kondisi'] = $kondisi;
+			}	
+			else{
+				$kondisi = 'admin';
+				$_SESSION['username'] = $query_result[0]['username'];
+				$_SESSION['password'] = $query_result[0]['password'];
+				$_SESSION['kondisi'] = $kondisi;
+			}
 		}
+		return $kondisi;
 		
 	}
 	public function logout(){
